5c5f9a05c27da3a2d1aa55a4bd7ae9e86c9fe9dc
Your Name <kuklinski.piotr89@gmail.com>
2024-01-30 16:33:03 +0100

    dwa testy nie przechodza

--- src/main/java/com/devskiller/tasks/blog/model/dto/PostDto.java
+++ src/main/java/com/devskiller/tasks/blog/model/dto/PostDto.java
@@ -2,5 +2,5 @@ package com.devskiller.tasks.blog.model.dto;
 
 import java.time.LocalDateTime;
 
-public record PostDto(String id, String title, String content, LocalDateTime creationDate) {
+public record PostDto(String title, String content, LocalDateTime creationDate) {
 }
